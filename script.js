767512e13d904b93f22452577d4ccb11e6299ff7
itsmichaeld <67594883+itsmichaeld@users.noreply.github.com>
2022-12-05 15:47:23 +0000

    Commit initial files

---
+++ script.js
@@ -0,0 +1,112 @@
+// Array of characters for password to choose from
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+const btn = document.getElementById("generate-password-btn");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+
+btn.addEventListener("click", function () {
+  let password = [];
+  for (let j = 0; j < 2; j++) {
+    for (let i = 0; i < 15; i++) {
+      let passwordChar = Math.floor(Math.random() * 91);
+      password.push(characters[passwordChar]);
+    }
+  }
+  let password1Content = password.slice(0, 15).join("");
+  let password2Content = password.slice(15, 30).join("");
+  password1.textContent = password1Content;
+  password2.textContent = password2Content;
+});
